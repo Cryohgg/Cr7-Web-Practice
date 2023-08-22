12f7e1d7831653c9c13ec2d6876ab511b7d17915
Lewis <123241448+Cryohgg@users.noreply.github.com>
2023-08-22 18:35:57 +0700

    header.js

--- header.js
+++ header.js
@@ -1,9 +1,12 @@
 const topDesignImg2 = document.getElementById("topDesignImg2")
 const topDesignArrow = document.getElementById("topDesignArrow")
+const thisIsText = document.getElementById("thisIsText")
 
 topDesignImg2.onmouseover = function() {
     topDesignArrow.classList.add("topDesignArrowHover")
+    thisIsText.classList.add("thisIsTextOp")
 }
 topDesignImg2.onmouseout = function() {
     topDesignArrow.classList.remove("topDesignArrowHover")
+    thisIsText.classList.remove("thisIsTextOp")
 }
